4f235e4780d0517fafe6c5ff248dc76a5c327920
Shokhrukh_Developer1 <shokhrukhdeveloper1@gmail.com>
2024-01-03 11:17:54 +0500

    try-catch added to the project

--- Program.cs
+++ Program.cs
@@ -7,37 +7,48 @@ namespace NEWKESCHA
   {
     static void Main()
     {
-      Animal kescha = new Bird(animalName: "Kescha", animalAge: 3);
-      Animal kasee = new Pet(animalName: "Kasee", animalAge: 5);
-
-
-      Console.Write("Enter your name: ");
-      string userName=Console.ReadLine();
-
-      Console.WriteLine("Kescha says:");
-      kescha.Greet(userName);
-      Console.WriteLine("Kasee says:");
-      kasee.Greet(userName);
-
-      string password="";
-      do
+      try
       {
-        Console.Write("Enter password: ");
-        password=Console.ReadLine();
-      }while(password!="123");
+        Animal kescha = new Bird(animalName: "Kescha", animalAge: 3);
+        Animal kasee = new Pet(animalName: "Kasee", animalAge: 5);
 
-      Console.Write("Enter your age: ");
-      string ageAsString = Console.ReadLine();
+        Console.Write("Enter your name: ");
+        string userName=Console.ReadLine();
 
-      Console.WriteLine("Converting...");
+        Console.WriteLine("Kescha says:");
+        kescha.Greet(userName);
+        Console.WriteLine("Kasee says:");
+        kasee.Greet(userName);
 
-      int userAge = Convert.ToInt32(ageAsString);
-      Console.WriteLine($"Succesfully converted! {userAge}");
+        string password="";
+        do
+        {
+          Console.Write("Enter password: ");
+          password=Console.ReadLine();
+        }while(password!="123");
 
-      kescha.CalculateAgeDifferenceWithNoReturn(userAge);
-      kescha.PrintAgeDifference();
-      kescha.CompareAges(userAge);
-      kescha.TellAboutFriends(userName, userAge);
+        Console.Write("Enter your age: ");
+        string ageAsString = Console.ReadLine();
+
+        Console.WriteLine("Converting...");
+
+        int userAge = Convert.ToInt32(ageAsString);
+        Console.WriteLine($"Succesfully converted! {userAge}");
+
+        kescha.CalculateAgeDifferenceWithNoReturn(userAge);
+        kescha.PrintAgeDifference();
+        kescha.CompareAges(userAge);
+        kescha.TellAboutFriends(userName, userAge);
+      }
+      catch(FormatException formatException)
+      {
+        Console.WriteLine($"Error was thrown. Message: {formatException.Message}");
+      }
+      finally
+      {
+        Console.WriteLine("Program ended.");
+      }
+
     }
   }
 }
